fa64b378f8a758ea9d9163ee634129a74bef7996
XyzonXD <131737930+Ramzgod@users.noreply.github.com>
2023-04-30 09:52:19 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1599,7 +1599,7 @@ def passwrd():
 	urut = []
 	print(M+' <------------------------------------------------------------------------------------->')
 	wa.print(Columns(urut))
-	cetak(panel(f'\t\x1b[1;96mHasil {h}OK{x} \x1b[1;96mTersimpan Di : {h}OK/%s\n {x}\x1b[1;96mHasil {k}CP{x} \x1b[1;96mTersimpan Di : {k}CP/%s {x}\n     {O}Mainkan Mode Pesawat Setiap {K2}1000 ID {O}Selama 10 Detik !'%,(okc))%%(cpc)),width=90,title=f"[bold red][[bold green] Tutorial Cookies [bold red]]",style=f"bold red")
+	cetak(panel(f'\t     {O}Mainkan Mode Pesawat Setiap {K}1000 ID {O}Selama 10 Detik !',width=90,title=f"[bold red][[bold green] Info Crack [bold red]]",style=f"bold red")
 	prog = Progress(SpinnerColumn('clock'),TextColumn('{task.description}'),BarColumn(),TextColumn('{task.percentage:.0f}%'))
 	des = prog.add_task('',total=len(id2))
 	with prog:
