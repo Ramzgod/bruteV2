633fc1872a1c84f630bd47221863bb08705d41c7
XyzonXD <131737930+Ramzgod@users.noreply.github.com>
2023-05-06 08:20:08 +0700

    Update run.py

--- run.py
+++ run.py
@@ -7,32 +7,30 @@
 # BANG BANG LU MAIN EPEP YA🗿
 # Recode Sc Orang Kagak Buat Lu Jago Dek-Dek
 #-----------------[ IMPORT - PREMIUM ]-------------------
-import requests,bs4,json,os,sys,random,datetime,time,re
-import urllib3,rich,base64
-from concurrent.futures import ThreadPoolExecutor as tred
-from rich.table import Table as me
-from rich.console import Console as sol
-from bs4 import BeautifulSoup as sop
-from bs4 import BeautifulSoup as parser
-from bs4 import BeautifulSoup as par
-from concurrent.futures import ThreadPoolExecutor as tred
-from rich.console import Group as gp
-from rich.panel import Panel as panel
-from rich.panel import Panel as nel
-from rich.progress import track
+import requests,bs4,json,os,sys,random,datetime,time,re,urllib3,rich,base64
 from time import sleep
-from rich import print as cetak
-from concurrent.futures import ThreadPoolExecutor as XyzonXD
-from rich.panel import Panel
-from rich.markdown import Markdown as mark
-from rich.columns import Columns as col
+from rich import pretty
 from rich.tree import Tree
+from rich.panel import Panel
+from rich import print as cetak
 from rich import print as rprint
 from rich import print as prints
-from rich import pretty
-from rich.console import Console as sol
-from rich.progress import Progress,SpinnerColumn,BarColumn,TextColumn
+from rich.progress import track
 from rich.text import Text as tekz
+from rich.console import Console
+from rich.columns import Columns
+from rich.panel import Panel as nel
+from rich.panel import Panel as panel
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup as par
+from rich.console import Group as gp
+from bs4 import BeautifulSoup as parser
+from rich.columns import Columns as col
+from rich.console import Console as sol
+from rich.markdown import Markdown as mark
+from concurrent.futures import ThreadPoolExecutor as tred
+from concurrent.futures import ThreadPoolExecutor as BrayennnXD 
+from rich.progress import Progress,SpinnerColumn,BarColumn,TextColumn
 dump = []
 memek = []
 ualu,ualuh = [],[]
@@ -57,15 +55,17 @@ except ImportError:
 	cetak(nel('\t• sedang menginstall Modul play-audio •'))
 	os.system('pkg install play-audio')
 	
-###----------[ IMPORT MODULE RICH ]---------- ###
-from rich.panel import Panel
-from rich.tree import Tree
-from rich import print as prints
-from rich.console import Console
-from rich.table import Table
-from rich.columns import Columns
-from rich.progress import Progress,SpinnerColumn,BarColumn,TextColumn,TimeElapsedColumn
+#------------------[ GLOBAL NAME ]-------------------#
+pretty.install()
+CON=sol()
+wa = Console()
+taplikasi=[]
+gabriel=[]
+BrayennnXD=[]
 console = Console()
+ses=requests.Session()
+id,id2,loop,ok,cp,akun,oprek,lisensiku,tokenku,uid,lisensikuni,method,pwpluss,pwnya= [],[],0,0,0,[],[],[],[],[],[],[],[],[]
+ugen2,ugen,dia,cokbrut,dump,memek,ualu,ualuh,lisensikuni,lisensiku,princp=[],[],[],[],[],[],[],[],[],[],[]
 sys.stdout.write('\x1b]2;XMBF | XYZON MULTI BRUTE FORCE\x07')
 #---------Tahun---------#
 def tahun(fx):
